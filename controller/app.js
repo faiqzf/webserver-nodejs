23053472f6bc7f502782125eab72876c5c8109ec
faiqzf <faiq.zafi.7.fz@gmail.com>
2020-03-05 19:19:17 +0700

    first commit

---
+++ controller/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var user = require('../model/user.js');
+
+app.get('/api/user/:userid', function (req, res) {
+    var id = req.params.userid;
+
+    user.getUser(id, function (err, result) {
+        if (!err) {
+            res.send(result);
+        } else {
+            res.status(500).send("Some error");
+        }
+    });
+
+});
+app.get('/api/user/:userid', function (req, res) {
+    
+
+    user.getUsers(id, function (err, result) {
+        if (!err) {
+            res.send(result);
+        } else {
+            res.status(500).send("Some error");
+        }
+    });
+
+});
+
+module.exports = app
